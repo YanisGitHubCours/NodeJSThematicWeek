cae81dc19f33cc1a408f82f0b7a9089fccd376a3
Yanis <yanis@Yone.local>
2022-03-30 12:36:37 +0200

    add morgan

--- server/index.mjs
+++ server/index.mjs
@@ -1,12 +1,14 @@
 import router from "./routes.mjs"
 import express from "express"
 import bodyparser from "body-parser";
+import morgan from "morgan"
 
 const port = 3000;
 const app = express()
 
 app.use(bodyparser.urlencoded({ extended: false }))
 app.use(bodyparser.json())
+app.use(morgan('combined'))
 
 app.use(router)
 
