1987953d9111e8a16e91ac0cadafbd1a371c5407
Yanis <yanis@Yone.local>
2022-03-31 10:55:52 +0200

    add function run with axios for request API

---
+++ client/index.mjs
@@ -0,0 +1,49 @@
+// DOC for IIFE: https://developer.mozilla.org/en-US/docs/Glossary/IIFE
+
+import axios from "axios"
+
+const getProducts = () => {
+    axios.get('http://localhost:3011/products')
+        .then((response) => {
+            console.log(response.data);
+            console.log(response.status);
+        });
+};
+
+const addProduct = () => {
+    axios.post('http://localhost:3011/products', {
+        name: 'pomme',
+        quantity: 5
+    })
+        .then((response) => {
+            console.log(response);
+        }, (error) => {
+            console.log(error);
+        });
+}
+
+const getProductsAsync = () => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            axios.get('http://localhost:3011/products')
+                .then((response) => {
+                    resolve(response.data);
+                });
+        }, 2000);
+    });
+}
+
+
+//IIFE
+(() => {
+    getProducts();
+    addProduct();
+})();
+
+//ASYNC IIFE
+(async () => {
+    const res = await getProductsAsync();
+    console.log(res)
+})();
+
+
