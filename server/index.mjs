87818c876f058d0107733cc90803ec56b0110685
Yanis <yanis@Yone.local>
2022-04-01 12:13:38 +0200

    add package swigger and api-docs

--- server/index.mjs
+++ server/index.mjs
@@ -2,6 +2,8 @@ import router from "./routes.mjs"
 import express from "express"
 import bodyparser from "body-parser";
 import morgan from "morgan"
+import swaggerUI from "swagger-ui-express"
+import docs from "../docs/index.mjs"
 
 const port = 3011;
 const app = express()
@@ -9,8 +11,9 @@ const app = express()
 app.use(bodyparser.urlencoded({ extended: false }))
 app.use(bodyparser.json())
 app.use(morgan('tiny'))
-
 app.use(router)
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(docs))
+
 
 app.listen(port, () => {
   console.log(`server listen at ${port}`)
